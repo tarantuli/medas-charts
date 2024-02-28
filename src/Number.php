b8d0f81aa55aa08a29e0dc985dfffde7fb80d6d6
Patrick Morpey <patrick@morphp.nl>
2024-02-28 10:16:58 +0100

    Added descriptions to constants

--- src/Number.php
+++ src/Number.php
@@ -6,15 +6,28 @@ namespace Medas\Charts;
 
 class Number
 {
+    /** A very small positive float that's used to compare numbers to zero */
     public const SMALL_POSITIVE = +1e-10;
+
+    /** A very small negative float that's used to compare numbers to zero */
     public const SMALL_NEGATIVE = -1e-10;
 
-    // Durations in seconds
+    /** The number of seconds in one day */
     public const ONE_DAY = 86400;
+
+    /** The number of seconds in one hour */
     public const ONE_HOUR = 3600;
+
+    /** The number of seconds in one minute */
     public const ONE_MINUTE = 60;
+
+    /** The number of seconds in 31 days */
     public const ONE_MONTH = 2678400;
+
+    /** The number of seconds in one week */
     public const ONE_WEEK = 604800;
+
+    /** The number of seconds in 366 days */
     public const ONE_YEAR = 31622400;
 
     public static function isMoreThanOrEqual(float $a, float $b): bool
